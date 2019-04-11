d70d076cb6d229b8ab3a1064b995b7328ddd443f
Stephanie Coy <scoy028@gmail.com>
2019-04-11 16:54:57 -0400

    lint fix

--- client/components/nav.js
+++ client/components/nav.js
@@ -5,22 +5,22 @@ export default class Nav extends Component {
     return (
       <div>
         <header>
-            <div className="full-width">
-                <div className="one-third-width">
-                    <h1>Stephanie Coy</h1>
-                </div>
-                <div className="two-thirds-width">
-                    <nav>
-                        <ul>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#work">Work</a></li>
-                        <li><a href="https://medium.com/@stephanie.a.coy">Blog</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                        </ul>
-                    </nav>
-                </div>
-                <h2><span>Hi,</span> I'm a new developer that loves clean & creative code.</h2>
+          <div className="full-width">
+            <div className="one-third-width">
+              <h1>Stephanie Coy</h1>
             </div>
+            <div className="two-thirds-width">
+              <nav>
+                  <ul>
+                  <li><a href="#about">About</a></li>
+                  <li><a href="#work">Work</a></li>
+                  <li><a href="https://medium.com/@stephanie.a.coy">Blog</a></li>
+                  <li><a href="#contact">Contact</a></li>
+                  </ul>
+              </nav>
+            </div>
+            <h2><span>Hi,</span> I'm a new developer that loves clean & creative code.</h2>
+          </div>
         </header>
       </div>
     )
--- client/index.js
+++ client/index.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
 import {Router} from 'react-router-dom'
 import history from './history'
 import Nav from './components/nav'
-import Main from './components/main'
+import Main from './main'
 
 ReactDOM.render(
-  <Provider>
     <Router history={history}>
       <Nav />
       <Main />
-    </Router>
-  </Provider>,
+    </Router>,
   document.getElementById('app')
 )
